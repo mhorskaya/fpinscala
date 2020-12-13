d2a90a7c226a28821b4572bc9cf2065b688be002
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-12-13 21:44:25 +0300

    refactor tests 2.1 and 2.2

--- src/test/scala/chapter02/Exercise02Spec.scala
+++ src/test/scala/chapter02/Exercise02Spec.scala
@@ -1,5 +1,6 @@
 package chapter02
 
+import chapter02.Exercise02.isSorted
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -7,14 +8,14 @@ class Exercise02Spec extends AnyFlatSpec with Matchers {
   val gt: (Int, Int) => Boolean = (x: Int, y: Int) => y > x
 
   "isSorted" should "detect that an array is sorted" in {
-    Exercise02.isSorted(Array(1, 2, 3, 4, 5), gt) should be(true)
+    isSorted(Array(1, 2, 3, 4, 5), gt) should be(true)
   }
 
   it should "detect that an array is not sorted" in {
-    Exercise02.isSorted(Array(1, 3, 2, 5, 4), gt) should be(false)
+    isSorted(Array(1, 3, 2, 5, 4), gt) should be(false)
   }
 
   it should "detect that an array with one element is sorted" in {
-    Exercise02.isSorted(Array(1), gt) should be(true)
+    isSorted(Array(1), gt) should be(true)
   }
 }
