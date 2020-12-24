be0f384d1588798528fe4b5d109b5a42ad967b9c
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2020-12-25 00:53:05 +0300

    exercises 4.1 through 4.5

---
+++ src/main/scala/chapter04/Option.scala
@@ -0,0 +1,74 @@
+package chapter04
+
+import scala.{Either => _, Option => _, Some => _, _}
+
+sealed trait Option[+A] {
+  // Exercise 01
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(a) => Some(f(a))
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(a) => f(a)
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(a) => a
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(a) if f(a) => this
+    case _ => None
+  }
+}
+
+case class Some[+A](get: A) extends Option[A]
+
+case object None extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  // Exercise 02
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+  // Exercise 03
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    (a, b) match {
+      case (Some(x), Some(y)) => Some(f(x, y))
+      case _ => None
+    }
+  }
+
+  def map3[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (x => b map (y => f(x, y)))
+
+  // Exercise 04
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_ :: _))
+
+  def sequence2[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => h flatMap (hh => sequence2(t) map (hh :: _))
+    }
+
+  // Exercise 05
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight[Option[List[B]]](Some(Nil))((x, y) => map2(f(x), y)(_ :: _))
+
+  def sequence3[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(x => x)
+}
