54995c4a02b8a32699466bb69b27fdad8ad8ed98
Mustafa Hakan Örskaya <mhorskaya@hotmail.com>
2021-01-02 16:28:12 +0300

    Exercises 6.01 through 6.04

---
+++ src/main/scala/chapter06/State.scala
@@ -0,0 +1,58 @@
+package chapter06
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+object RNG {
+
+  case class Simple(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = Simple(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
+
+  // Exercise 01
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
+
+  // Exercise 02
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
+
+  // Exercise 03
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
+
+  // Exercise 04
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+    count match {
+      case 0 => (List(), rng)
+      case _ =>
+        val (x, r1) = rng.nextInt
+        val (xs, r2) = ints(count - 1)(r1)
+        (x :: xs, r2)
+    }
+}
